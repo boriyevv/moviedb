b4d935f6e29f2eac71df57fc7026c5927cf7102f
boriyevv <boriyevdev08@gmail.com>
2024-11-12 17:57:13 +0500

    some feat added

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 let wrapper = document.querySelector('.wrapper')
 let category = document.querySelectorAll('.btn')
 let title = document.querySelector('.title')
+let inp = document.querySelector('.inp')
+let modalList = document.querySelector('.modal-list')
+let form = document.querySelector('.form')
+let select = form.select
 
 
 
-category.forEach(item=>{
-    item.addEventListener('click', ()=>{
+category.forEach(item => {
+    item.addEventListener('click', () => {
         getdata(item.dataset.key)
-        title.textContent = `${(item.dataset.key).charAt(0).toUpperCase()+(item.dataset.key).slice(1)}  Movies`
+        title.textContent = `${(item.dataset.key).charAt(0).toUpperCase() + (item.dataset.key).slice(1)}  Movies`
     })
 })
 
 
-async function getdata(key='popular') {
+async function getdata(key = 'popular', page = 2) {
     let data = await fetch(`https://api.themoviedb.org/3/movie/${key}?language=en-US&page=1`, {
         method: 'GET',
         headers: {
@@ -22,41 +26,109 @@ async function getdata(key='popular') {
     })
     let parsedata = await data.json()
     render(parsedata.results)
+
+    inp.addEventListener('input', () => {
+
+        let filtered = parsedata.results.filter(movie => movie.original_title.toLowerCase().includes(inp.value.toLowerCase()))
+        console.log(filtered)
+        render(filtered)
+    })
+
+    let sorted = []
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        if (select.value == 'kop') {
+            sorted = parsedata.results.sort((a, b) => b.popularity - a.popularity)
+            // console.log(sorted)
+        }
+
+        else if (select.value == 'baho') {
+            sorted = parsedata.results.sort((a, b) => b.vote_average - a.vote_average)
+            // console.log(sorted)
+        }
+
+        else if (select.value == 'yangi') {
+            sorted = parsedata.results.sort((a, b) => parseInt(b.release_date.slice(0, 4)) - parseInt(a.release_date.slice(0, 4)))
+            // console.log(sorted)
+        }
+        else if (select.value == 'yangi') {
+            sorted = parsedata.results.sort((a, b) => parseInt(a.release_date.slice(0, 4)) - parseInt(b.release_date.slice(0, 4)))
+            // console.log(sorted)
+
+        }
+        render(sorted)
+        console.log(sorted)
+
+        
+    })
+
+
 }
 getdata()
 
+let favouriteMovies = []
 
 
+function render(movies) {
+    wrapper.innerHTML = null
 
+    for (let i=0; i<movies.length; i++) {
+        // console.log(i)
+        let card = document.createElement('div')
+        card.setAttribute('style', 'width: 18rem;')
+        card.setAttribute('class', 'card')
 
-
-    function render(movies){
-        wrapper.innerHTML = null
-
-        for(let i of movies){
-            // console.log(i)
-            let card = document.createElement('div')
-            card.setAttribute('style', 'width: 18rem;')
-            card.setAttribute('class', 'card')
-
-            card.innerHTML = `
+        card.innerHTML = `
             
-                    <img src="https://media.themoviedb.org/t/p/w200${i.poster_path}" class="card-img-top" alt="...">
+                    <img src="https://media.themoviedb.org/t/p/w200${movies[i].poster_path}" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">${i.original_title}</h5>
-                        <p class="card-text">${i.overview}</p>
+                        <h5 class="card-title">${movies[i].original_title}</h5>
+                        <p class="card-text">${movies[i].overview}</p>
                         </div>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <span class="d-flex gap-2 p-2">
+                            <a href="#" class="btn btn-primary w-50"><i class="fa-solid fa-play"></i>
+                            Watch Movie</a>
+                            <button onclick="addLikes('${movies[i].original_title}')" class="btn btn-danger  w-50"><i class="fa-solid fa-heart text-light"></i> Add to favourites</button>
+                        </span>
 
 
             `
 
-            wrapper.append(card)
-
-        }
-
+        wrapper.append(card)
 
     }
 
-    render()
+
+}
+
+render()
+
+
+
+function addLikes(arr){
+    console.log(arr)
+
+    favouriteMovies.push(arr)
+
+    localStorage.setItem('movies', JSON.stringify(favouriteMovies))
+
+    localRender()
+}
+
+function localRender(){
+    let localMovies = localStorage.getItem('movies') ? JSON.parse(localStorage.getItem('movies')) : []
+
+    modalList.innerHTML = null
+
+    for(i of localMovies){
+        console.log(i)
+        let li = document.createElement('li')
+
+        li.textContent = i
+
+        modalList.append(i)
+    }
+
+}
 
